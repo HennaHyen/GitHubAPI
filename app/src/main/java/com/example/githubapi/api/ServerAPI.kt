55cdb2d405c628cd5feb9c097e7c3b771bb3cfb5
HennaHyen <henna.hyen97@kakaovx.com>
2022-09-02 22:05:57 +0900

    [feat] Retrofit 객체 생성 (#2)
    retrofit 객체 생성

--- app/src/main/java/com/example/githubapi/api/ServerAPI.kt
+++ app/src/main/java/com/example/githubapi/api/ServerAPI.kt
@@ -1,10 +1,19 @@
 package com.example.githubapi.api
 
+import android.content.Context
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ServerAPI {
     companion object {
         private val baseUrl = "https://api.github.com"
         private var retrofit: Retrofit? = null
+
+        fun getRetrofit(context: Context) {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
     }
 }
